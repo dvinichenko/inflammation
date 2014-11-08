0a41e6c932ef43c55899f714ecd52c833cd8cd0f
Dmitry Vinichenko <dvinichenko@fas.harvard.edu>
2014-11-08 16:26:31 -0500

    final commit

--- src/add_data.sh
+++ src/add_data.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-DRUG=$(python assign_drug.py $2)
-DEST="../data/$1/$1-$DRUG.dat"
-mkdir -p ../data/$1
+DRUG=$(python src/assign_drug.py $2)
+CURR_DATE=$(date +%Y-%m-%d)
+DEST_DIR="$3/data/$CURR_DATE"
+TARGET_FNAME="$CURR_DATE-$DRUG.dat"
 
-mv $2 $DEST
+mkdir -p $DEST_DIR
+mv $2 $DEST_DIR/$TARGET_FNAME
 
 # Add and commit to git repo
 
-MSG="Acquired $DEST"
-git add $DEST
+MSG="Acquired $TARGET_FNAME"
+git add $DEST_DIR/$TARGET_FNAME
 git commit -m "'$MSG'"
-echo "Finished gracefully with adding $DEST"
+echo "Finished gracefully with adding $TARGET_FNAME"
